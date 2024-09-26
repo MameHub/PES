e492346a15495318bd5fc662238c19e74bbb6187
Álvaro Cañas <acaygo@gmail.com>
2024-09-26 10:03:24 +0200

    print

--- Unidad_3/ej2.php
+++ Unidad_3/ej2.php
@@ -16,59 +16,59 @@ $anno = 2024;
 switch(mes) {
 
     case 1:
-        echo"Enero tiene 31 días.";
+        print"Enero tiene 31 días.";
     break;
 
     case 2:
         if(anno % 4 == 0) {
-            echo"Febrero tiene 29 días.";
+            print"Febrero tiene 29 días.";
         } else {
-            echo"Febrero tiene 28 días.";
+            print"Febrero tiene 28 días.";
         }
     break;
 
     case 3:
-        echo"marzo tiene 31 días.";
+        print"marzo tiene 31 días.";
     break;
 
     case 4:
-        echo"Abril tiene 30 días.";
+        print"Abril tiene 30 días.";
     break;
 
     case 5:
-        echo"Mayo tiene 31 días.";
+        print"Mayo tiene 31 días.";
     break;
 
     case 6:
-        echo"Junio tiene 30 días.";
+        print"Junio tiene 30 días.";
     break;
 
     case 7:
-        echo"Julio tiene 31 días.";
+        print"Julio tiene 31 días.";
     break;
 
     case 8:
-        echo"Agosto tiene 31 días.";
+        print"Agosto tiene 31 días.";
     break;
 
     case 9:
-        echo"Septiembre tiene 30 días.";
+        print"Septiembre tiene 30 días.";
     break;
 
     case 10:
-        echo"Octubre tiene 31 días.";
+        print"Octubre tiene 31 días.";
     break;
 
     case 11:
-        echo"Noviembre tiene 30 días.";
+        print"Noviembre tiene 30 días.";
     break;
 
     case 12:
-        echo"Diciembre tiene 31 días.";
+        print"Diciembre tiene 31 días.";
     break;
-    
+
     default:
-        echo"ERROR.";
+        print"ERROR.";
 
 };
 
