d5daeb335ae54f4512410c1a6849bc4a51af8682
Álvaro Cañas <acaygo@gmail.com>
2024-09-30 01:50:23 +0200

    ejercicios ud3

---
+++ Unidad_3/Actividad_1/ej4.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Modifica la página inicial realizada, incluyendo una imagen de cabecera en función de la
+estación del año en la que nos encontremos y un color de fondo en función de la hora del día.
+*/
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 4</title>
+</head>
+<body>
+<?php
+
+?>
+</body>
+</html>
---
+++ Unidad_3/Actividad_1/ej5.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Script que muestre una lista de enlaces en función del perfil de usuario:
+    Perfil Administrador: Pagina1, Pagina2, Pagina3, Pagina4
+    Perfil Usuario: Pagina1, Pagina2
+*/
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 5</title>
+</head>
+<body>
+<?php
+
+?>
+</body>
+</html>
