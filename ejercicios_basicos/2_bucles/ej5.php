d4374d37fbc5a5447983eeb8371ebcfbc2a4a24a
MameHub <acaygo@gmail.com>
2024-12-09 23:02:30 +0000

    ej5 bucles

--- ejercicios_basicos/2_bucles/ej5.php
+++ ejercicios_basicos/2_bucles/ej5.php
@@ -6,6 +6,8 @@ Ejercicio 5. Factorial de un número:
     Calcula el factorial de un número dado usando un bucle for.
 */
 
+$factorial = 9;
+$resultado = 1;
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -16,7 +18,12 @@ Ejercicio 5. Factorial de un número:
 </head>
 <body>
     <?php
-    
+    for ($i = 1; $i <= $factorial; $i++) { 
+        $resultado *= $i;
+    }
+    echo "El factorial de $factorial es $resultado";
     ?>
+    <br/>
+    <a href="https://github.com/MameHub/PES/blob/main/ejercicios_basicos/2_bucles/ej5.php">Ejercicio en GitHub</a>
 </body>
 </html>
