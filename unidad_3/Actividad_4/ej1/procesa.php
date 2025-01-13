6bd7ea3216a93cd0e5562dbef11472c04e85af50
MameHub <acaygo@gmail.com>
2025-01-13 23:27:48 +0000

    ah

---
+++ unidad_3/Actividad_4/ej1/procesa.php
@@ -0,0 +1,16 @@
+<?php
+$name = $_POST['name'];
+$surname = $_POST['surname'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$studies = $_POST['studies'];
+echo "Nombre: " . $name;
+echo "<br/>";
+echo "Apellidos: " . $surname;
+echo "<br/>";
+echo "Email: " . $email;
+echo "<br/>";
+echo "Telefono: " . $phone;
+echo "<br/>";
+echo "Estudios: " . $studies;
+?>
--- unidad_3/Actividad_4/ej3/procesa.php
+++ unidad_3/Actividad_4/ej3/procesa.php
@@ -1,3 +1,15 @@
 <?php
+$name = $_POST['name'];
+$surname = $_POST['surname'];
+$email = $_POST['email'];
+$sex = $_POST['sex'];
 
+echo 'Nombre: ' . $name;
+echo '<br/>';
+echo 'Apellidos: ' . $surname;
+echo '<br/>';
+echo 'Email: ' . $email;
+echo '<br/>';
+echo 'Sexo: ' . $sex;
+echo '<br/>';
 ?>
