aabef2694bcfae6678a71912313e3bf54e9176e5
Álvaro Cañas <acaygo@gmail.com>
2025-03-30 19:41:03 +0200

    ah

---
+++ recuperaciones/unidad_3/2_tableroDamas/index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style/style.css">
+    <title>Álvaro Cañas</title>
+</head>
+<body>
+
+<header>
+
+    <h1>Tablero ajedrez</h1>
+    <hr>
+
+</header>
+
+<main>
+
+    <?php
+
+    $letras = ["a","b","c","d","e","f","g","h"];
+    $numeros = [8,7,6,5,4,3,2,1];
+    
+    echo '<table class="tablero">';
+    
+    echo '<tr><td></td>';
+
+    foreach ($letras as $fila) {
+        echo '<td>',$fila,'</td>';
+    }
+
+    // FILAS
+    for($i = 0; $i < 8; $i++) {
+
+        echo '<tr>';
+
+        echo '<td>',$numeros[$i],'</td>';
+
+        // COLUMNAS
+        for($j = 0; $j < 8; $j++) {
+
+            if($i % 2 == 0 && $j % 2 == 0 || $i % 2 != 0 && $j % 2 != 0) {
+                $color = "blanco";
+                
+            } else {
+                $color = "negro";
+            }
+            
+            echo '<td class=',$color,'></td>';
+
+        }
+
+        echo '<td>',$numeros[$i],'</td>';
+        
+        echo '</tr>';
+
+    }
+
+    echo '<tr>';
+
+    echo '<tr><td></td>';
+
+    foreach ($letras as $fila) {
+        echo '<td>',$fila,'</td>';
+    }
+
+    echo '</tr>';
+    
+    echo '</table>';
+
+    ?>
+
+</main>
+
+<footer>
+
+    <hr>
+    <h2>Actividad 1. Tablero de ajedrez</h2>
+    <a href="https://github.com/MameHub/PES/tree/main/tableroAjedrez" target="_blank">Enlace a github</a>
+    <h3>Álvaro Cañas González</h3>
+    <h3>Programación en entorno servidor</h3>
+
+</footer>
+
+</body>
+</html>
