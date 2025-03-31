9dea0f5be3779d9102e48ed57c9230ed0ea06a34
Álvaro Cañas <acaygo@gmail.com>
2025-03-31 13:44:51 +0200

    creación actividad calendario

---
+++ recuperaciones/unidad_3/3_calendario/index.php
@@ -0,0 +1,72 @@
+<?php
+        
+// Variables.
+$anno = date('Y');
+$mes = date('m');
+$dias;
+$diasSemana = ["lunes", "martes", "miercoles", "jueves", "viernes", "sábado", "domingo"];
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style/style.css">
+    <title>Álvaro Cañas</title>
+</head>
+<body>
+
+    <header>
+
+    <h1>Calendario</h1>
+    <hr>
+
+    </header>
+
+    <main>
+
+    <p>Ejercicio 5. Dado el mes y el año almacenados en variables, escribir un programa que muestre el calendario mensual correspondiente. Marcar el día actual en verde y los festivos en rojo.</p>
+
+        <?php
+
+            // echo '<table>';
+            
+            // echo '<tr>';
+            
+            // for ($i=0; $i < count($diasSemana); $i++) { 
+            //     echo '<td>',$diasSemana[$i],'</td>';
+            // }
+            
+            // echo '</tr>';
+
+            // echo '</table>';
+
+            for ($i=1; $i <= $nHueco; $i++) { 
+                echo 'b';
+            }
+
+            for ($i=1; $i <= $nDiasMes; $i++) { 
+                echo $i;
+                if ($i == 7) {
+                    echo '</br>';
+                }
+            }
+
+        ?>
+
+    </main>
+
+    <footer>
+
+    <hr>
+    <h2>Actividad 3. Ejercicio 5 de bucles</h2>
+    <a href="" target="_blank">Enlace a github</a>
+    <h3>Álvaro Cañas González</h3>
+    <h3>Programación en entorno servidor</h3>
+
+    </footer>
+
+</body>
+</html>
