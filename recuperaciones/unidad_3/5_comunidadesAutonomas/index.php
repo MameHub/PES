546034c503a1d9e99d8490b9e2c3d1ab46e9017c
Álvaro Cañas <acaygo@gmail.com>
2025-04-11 02:29:43 +0200

    generacion aleatoria

--- recuperaciones/unidad_3/5_comunidadesAutonomas/index.php
+++ recuperaciones/unidad_3/5_comunidadesAutonomas/index.php
@@ -1,27 +1,31 @@
 <?php
 
 # LISTA DE COMUNIDADES AUTONOMAS CON SUS PROVINCIAS -> https://www.mundodeportivo.com/uncomo/educacion/articulo/cuantas-comunidades-autonomas-hay-en-espana-42821.html
-# Array asociativo multidimensional con las Comunidades autónomas y sus provincias.
+# Variables.
+# Clases estilos.
+$correcto = "green";
+$incorrecto = "red";
+# Array asociativo con las Comunidades autónomas y sus provincias.
 $comunidades = [
-    ['Andalucía' => ['Almería', 'Cádiz', 'Córdoba', 'Granada', 'Huelva', 'Jaén', 'Málaga', 'Sevilla']],
-    ['Extremadura' => ['Badajoz', 'Cáceres']],
-    ['Castilla y león' => ['Ávila', 'Burgos', 'León', 'Salamanca', 'Segovia', 'Soria', 'Palencia', 'Valladolid', 'Zamora']],
-    ['Galicia' => ['A Coruña', 'Ourense', 'Pontevedra', 'Lugo']],
-    ['Principado de Asturias' => ['Asturias']],
-    ['Cantabria' => ['Cantabria']],
-    ['País Vasco' => ['Álava', 'Guipuzcoa', 'Vizcaya']],
-    ['Comunidad foral de Navarra' => ['Navarra']], 
-    ['Aragón' => ['Huesca', 'Teruel', 'Zaragoza']], 
-    ['Cataluña' => ['Barcelona', 'Girona', 'Lleida', 'Tarragona']], 
-    ['Comunidad valenciana' => ['Alicante', 'Castellón', 'Valencia']],
-    ['Región de Murcia' => ['Murcia']],
-    ['Castilla la Mancha' => ['Albacete', 'Ciudad Real', 'Cuenca', 'Guadalajara', 'Toledo']],
-    ['Comunidad de Madrid' => ['Madrid']], 
-    ['La Rioja' => ['La rioja']], 
-    ['Ceuta' => ['Ceuta']], 
-    ['Melilla' => ['Melilla']], 
-    ['Islas Baleares' => ['Ibiza', 'Mallorca', 'Menorca']],
-    ['Islas Canarias' => ['Fuerteventura', 'Gran Canaria', 'Lanzarote', 'El Hierro', 'La Gomera', 'La Palma', 'Tenerife']]
+    'Andalucía' => ['Almería', 'Cádiz', 'Córdoba', 'Granada', 'Huelva', 'Jaén', 'Málaga', 'Sevilla'],
+    'Extremadura' => ['Badajoz', 'Cáceres'],
+    'Castilla y león' => ['Ávila', 'Burgos', 'León', 'Salamanca', 'Segovia', 'Soria', 'Palencia', 'Valladolid', 'Zamora'],
+    'Galicia' => ['A Coruña', 'Ourense', 'Pontevedra', 'Lugo'],
+    'Principado de Asturias' => ['Asturias'],
+    'Cantabria' => ['Cantabria'],
+    'País Vasco' => ['Álava', 'Guipuzcoa', 'Vizcaya'],
+    'Comunidad foral de Navarra' => ['Navarra'], 
+    'Aragón' => ['Huesca', 'Teruel', 'Zaragoza'], 
+    'Cataluña' => ['Barcelona', 'Girona', 'Lleida', 'Tarragona'], 
+    'Comunidad valenciana' => ['Alicante', 'Castellón', 'Valencia'],
+    'Región de Murcia' => ['Murcia'],
+    'Castilla la Mancha' => ['Albacete', 'Ciudad Real', 'Cuenca', 'Guadalajara', 'Toledo'],
+    'Comunidad de Madrid' => ['Madrid'], 
+    'La Rioja' => ['La rioja'], 
+    'Ceuta' => ['Ceuta'], 
+    'Melilla' => ['Melilla'], 
+    'Islas Baleares' => ['Ibiza', 'Mallorca', 'Menorca'],
+    'Islas Canarias' => ['Fuerteventura', 'Gran Canaria', 'Lanzarote', 'El Hierro', 'La Gomera', 'La Palma', 'Tenerife']
 ];
 
 # Comprobación de envío de formulario
@@ -29,9 +33,16 @@ $procesaFormulario = false;
 if (isset($_POST['enviar'])) {
     $procesaFormulario = true;
 }
-
+# Recepción de formulario
 if ($procesaFormulario) {
-    echo rand($comunidades(0), end($comunidades));
+    # Comprobamos que el formulario haya sido enviado para evitar fallos en la recarga[...].
+    if ($_POST['validate'] == 1) {
+        # Mostramos el resultado.
+
+
+        # echo '<p>',$provinciaRandom,'</p>';
+        echo $_POST;
+    }
 }
 
 ?>
@@ -53,33 +64,27 @@ if ($procesaFormulario) {
     <form action="">
         
     <?php
-    
-    
-    // $n = rand($comunidades(0), end($comunidades));
 
-    // echo $comunidades[$n];
+    # Mostramos una provicia de forma aleatoria.
+    echo '<label>',array_rand($comunidades, 1),'</label>' #Corregir, solo muestra comunidades en vez de provincias.
 
     ?>
             
         <select>
 
-            <!-- <option> -->
-
                 <?php
                 
-                foreach ($comunidades as $comunidad) {
-                    foreach ($comunidad as $nombre => $provincias) {
-                        foreach ($provincias as $provincia) {
-                            echo '<option>',$provincia,'</option>';
-                        }
-                    }
+                # Mostramos un desplegable con las comunidades autónomas.
+                foreach ($comunidades as $comunidad => $provincia) {
+                    echo '<option>',$comunidad,'</option>';
                 }
 
                 ?>
 
-            <!-- </option> -->
-
         </select>
+
+        <input type="hidden" name="validate" value="1">
+
         <button type="submit" name="enviar">Enviar</button>
 
     </form>
