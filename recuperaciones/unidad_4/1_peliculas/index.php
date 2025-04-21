cbd7b856eabe91ee4b8fa99bc9d17b8124a0e256
Álvaro Cañas <acaygo@gmail.com>
2025-04-21 13:03:44 +0200

    subida ejercicio

---
+++ recuperaciones/unidad_4/1_peliculas/index.php
@@ -0,0 +1,42 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style/style.css">
+    <title>Álvaro Cañas</title>
+</head>
+<body>
+    <header>
+        <h1>Cinemax</h1>
+        <nav>
+            <ul>
+                <li><a href="">Inicio</a></li>
+                <li><a href="">Acción</a></li>
+                <li><a href="">Aventuras</a></li>
+                <li><a href="">Ciencia ficción</a></li>
+                <li><a href="">Drama</a></li>
+            </ul>
+        </nav>
+        <hr>
+    </header>
+    <main>
+    
+    <?php
+
+    ?>
+    
+    </main>
+    <footer>
+        <hr>
+        <h2>Programación en entorno servidor</h2>
+        <h2>Unidad 4. Programación en entorno servidor</h2>
+        <h3>Álvaro Cañas González</h3>
+        <a href="">Enlace a GitHub</a>
+    </footer>
+</body>
+</html>
