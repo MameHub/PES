9d948c3bd8364de057d0484047cc13eb1c360f04
Álvaro Cañas <acaygo@gmail.com>
2025-05-03 18:00:01 +0200

    ej1 clase cookies

---
+++ recuperaciones/unidad_4/actividadesClase/ejercicio1.php
@@ -0,0 +1,31 @@
+
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1</title>
+</head>
+<body>
+    
+    <header>
+        <h1>Actividades de clase</h1>
+        <h2>Unidad 4 - Cookies</h2>
+        <ol>
+            <li>Escriba una página que permita crear una cookie de duración limitada, comprobar el estado de la cookie y destruirla.</li>
+        </ol>
+    </header>
+    <main>
+    <?php
+    
+    ?>
+    </main>
+    <footer>
+        <h2>Álvaro Cañas González</h2>
+        <h3>Programación en entorno servidor</h3>
+        <H3>Unidad 4 - Programación en entorno servidor [Cookies]</H3>
+        <a href="">Enlace a GitHub</a>
+    </footer>
+</body>
+</html>
