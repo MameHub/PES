04786b4cb9113ba3b24d7f807ff7085766ed343d
Álvaro Cañas <acaygo@gmail.com>
2025-05-03 21:52:57 +0200

    nav y peliculas

--- recuperaciones/unidad_4/1_peliculas/index.php
+++ recuperaciones/unidad_4/1_peliculas/index.php
@@ -5,7 +5,7 @@
  * ENUNCIADO:
  * Crear una aplicación en PHP que almacene en un array un listado de películas organizadas por género, con menú de acceso y preferencias.
  * REQUISITOS:
- * · Definir un array para almacenar pelíuclas y género. Guardaremos imagen de la carátula de la película y año de la misma.
+ * · Definir un array para almacenar películas y género. Guardaremos imagen de la carátula de la película y año de la misma.
  * · Crear una aplicación con menú para acceder a las películas por género. 
  * · Permitir al usuario marcar preferencias de género.
  * · La página de inicio muestra al usuario las películas de su preferencia. Si el usuario no tiene preferencias, se mostrarán las películas del año actual.
@@ -13,13 +13,35 @@
  * Aplicación HTML con menú de navegación y opción de preferencias de usuario.
  */
 
- # VARIABLES.
- $year = 2025;
- $aPeliculas = [
-    ['titulo' => 'El último samurai', 'anno' => 2003, 'genero' => ['Aventuras', 'Accion', 'Drama'], 'caratula' => 'https://es.web.img3.acsta.net/medias/nmedia/18/67/78/13/20066706.jpg'],
+# VARIABLES.
+$year = 2025;
+$aPeliculas = [
+    ['titulo' => 'El último samurai', 'anno' => 2003, 'genero' => ['Aventuras', 'Accion', 'Drama'], 'caratula' => 'https://pics.filmaffinity.com/the_last_samurai-270445769-large.jpg'],
     ['titulo' => 'Intocable', 'anno' => 2011, 'genero' => ['Comedia', 'Drama'], 'caratula' => 'https://pics.filmaffinity.com/Intocable-218166359-large.jpg'],
-    ['titulo' => 'Interestelar', 'anno' => 2014, 'genero' => ['Ciencia ficcion', 'Aventuras', 'Drama'], 'caratula' => 'https://m.media-amazon.com/images/M/MV5BYzdjMDAxZGItMjI2My00ODA1LTlkNzItOWFjMDU5ZDJlYWY3XkEyXkFqcGc@._V1_.jpg']
+    ['titulo' => 'Interestelar', 'anno' => 2014, 'genero' => ['Ciencia ficcion', 'Aventuras', 'Drama'], 'caratula' => 'https://m.media-amazon.com/images/M/MV5BYzdjMDAxZGItMjI2My00ODA1LTlkNzItOWFjMDU5ZDJlYWY3XkEyXkFqcGc@._V1_.jpg'],
+    ['titulo' => 'Chappie', 'anno' => 2015, 'genero' => ['Accion', 'Ciencia Ficcion'], 'caratula' => 'https://m.media-amazon.com/images/M/MV5BMTUyNTI4NTIwNl5BMl5BanBnXkFtZTgwMjQ4MTI0NDE@._V1_.jpg'],
+    ['titulo' => 'Joker', 'anno' => 2019, 'genero' => ['Thriller', 'Crimen'], 'caratula' => 'https://pics.filmaffinity.com/joker-790658206-large.jpg'],
+    ['titulo' => 'Robots', 'anno' => 2005, 'genero' => ['Infantil', 'Aventuras'], 'caratula' => 'https://pics.filmaffinity.com/Robots-967630798-large.jpg'],
+    ['titulo' => 'The Dark Knight', 'anno' => 2008, 'genero' => ['Thriller', 'Accion'], 'caratula' => 'https://pics.filmaffinity.com/the_dark_knight-102763119-large.jpg'],
+    ['titulo' => 'Pulp Fiction', 'anno' => 1994, 'genero' => ['Thriller', 'Crimen'], 'caratula' => 'https://pics.filmaffinity.com/pulp_fiction-210382116-large.jpg'],
+    ['titulo' => 'Inception', 'anno' => 2010, 'genero' => ['Ciencia ficcion', 'Thriller'], 'caratula' => 'https://pics.filmaffinity.com/inception-652954101-large.jpg'],
+    ['titulo' => 'Mickey 17', 'anno' => 2025, 'genero' => ['Ciencia ficcion', 'Comedia'], 'caratula' => 'https://pics.filmaffinity.com/mickey_17-530854063-large.jpg'],
+    ['titulo' => 'Los Cuatro Fantásticos: Primeros pasos', 'anno' => 2025, 'genero' => ['Accion', 'Fantastico'], 'caratula' => 'https://pics.filmaffinity.com/the_fantastic_four_first_steps-302831708-large.jpg'],
+    ['titulo' => 'Frankenstein', 'anno' => 2025, 'genero' => ['Terror', 'Drama'], 'caratula' => 'https://pics.filmaffinity.com/frankenstein-629082528-large.jpg'],
+    ['titulo' => 'Niños grandes', 'anno' => 2010, 'genero' => ['Comedia', 'Familia'], 'caratula' => 'https://pics.filmaffinity.com/grown_ups-567297059-large.jpg'],
+    ['titulo' => 'Scarface', 'anno' => 1983, 'genero' => ['Drama', 'Mafia'], 'caratula' => 'https://pics.filmaffinity.com/scarface-798722679-large.jpg'],
+    ['titulo' => 'El corredor del laberito', 'anno' => 2014, 'genero' => ['Ciencia ficcion', 'Accion'], 'caratula' => 'https://pics.filmaffinity.com/the_maze_runner-377064363-large.jpg']
 ];
+$aGeneros = array();
+
+# Obtenemos los géneros de las películas.
+foreach ($aPeliculas as $pelicula) {
+    $aGeneros = array_merge($aGeneros, $pelicula['genero']);
+}
+# Eliminamos los duplicados.
+$aGeneros = array_unique($aGeneros);
+# Ordenaos alfabéticamente.
+sort($aGeneros);
 
 ?>
 
@@ -37,29 +59,35 @@
         <nav>
             <ul>
                 <li><a href="">Inicio</a></li>
-                <li><a href="">Acción</a></li>
-                <li><a href="">Aventuras</a></li>
-                <li><a href="">Ciencia ficción</a></li>
-                <li><a href="">Drama</a></li>
+                <!-- Generamos el menú mediante un array que contiene los generos previamente procesado para ordenadrlo alfabéticamente y evitar los repetidos. -->
+                <?php
+                    foreach ($aGeneros as $generos) {
+                        echo '<li><a href="">',$generos,'</a></li>';
+                    }
+                ?>
             </ul>
         </nav>
         <hr>
     </header>
     <main>
-    
-    <div>
-        <img src="https://es.web.img3.acsta.net/medias/nmedia/18/67/78/13/20066706.jpg" alt="Caratula el último samurai" width="240">
-        <h2>El último samurai</h2>
-    </div>
-
     <div class="peliculas">
-        
-        <?php
 
-        ?>
+    <?php
+    
+    foreach ($aPeliculas as $peliculas) {
+        // if ($peliculas['anno'] == 2025) {
+        if (in_array('Accion', $peliculas['genero'])) {
+            echo '<div class="pelicula">';
+            echo '<img src="',$peliculas['caratula'],'" alt="Caratula ',$peliculas['titulo'],'" width=240>';
+            echo '<h2>',$peliculas['titulo'],'</h2>';
+            echo '</div>';
+            echo '</br>';
+        }
+    }
+
+    ?>
 
     </div>
-    
     </main>
     <footer>
         <hr>
