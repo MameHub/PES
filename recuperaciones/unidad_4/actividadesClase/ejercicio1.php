4a7130983cd899b972549a0ef1933b0bbd3a8fbc
Álvaro Cañas <acaygo@gmail.com>
2025-05-03 19:25:40 +0200

    prueba de cookies

--- recuperaciones/unidad_4/actividadesClase/ejercicio1.php
+++ recuperaciones/unidad_4/actividadesClase/ejercicio1.php
@@ -1,4 +1,44 @@
+<?php
 
+    # Variables
+    
+
+    # Creamos la cookie.
+    // setcookie("cookie", "cookie de duración limitada", time() + 10);
+
+    /**
+     * 
+     */
+    function creacionCookie($nombre, $valor, $duracion) {
+        if (!isset($_COOKIE[$nombre])) {
+
+            setcookie($nombre, $valor, time() + $duracion);
+            
+            for ($i = $duracion; $i > 0; $i--) { 
+                echo $i;
+            }
+
+        } else {
+            echo '<p>Ya hay una cookie con ese nombre</p>';
+        }
+        
+    }
+
+    /**
+     * 
+     */
+    function comprobacionCookie($nombre) {
+
+    }
+
+    /**
+     * 
+     */
+    function eliminacionCookie($nombre) {
+
+    }
+
+?>
 
 <!DOCTYPE html>
 <html lang="es">
@@ -8,7 +48,6 @@
     <title>Ejercicio 1</title>
 </head>
 <body>
-    
     <header>
         <h1>Actividades de clase</h1>
         <h2>Unidad 4 - Cookies</h2>
@@ -18,14 +57,23 @@
     </header>
     <main>
     <?php
-    
+
+        creacionCookie("cookie", "duracion de 10 segundos", 10);
+        
+        # Mostramos una cookie en caso de que exista.
+        if (isset($_COOKIE['cookie'])) {
+            echo $_COOKIE["cookie"];
+        } else {
+            echo '<p>No se encuentra ninguna cookie.</p>';
+        }
+
     ?>
     </main>
     <footer>
-        <h2>Álvaro Cañas González</h2>
-        <h3>Programación en entorno servidor</h3>
-        <H3>Unidad 4 - Programación en entorno servidor [Cookies]</H3>
-        <a href="https://github.com/MameHub/PES/blob/9d948c3bd8364de057d0484047cc13eb1c360f04/recuperaciones/unidad_4/actividadesClase/ejercicio1.php" target="_blank">Enlace a GitHub</a>
+            <h2>Álvaro Cañas González</h2>
+            <h3>Programación en entorno servidor</h3>
+            <h3>Unidad 4 - Programación en entorno servidor [Cookies]</h3>
+            <a href="https://github.com/MameHub/PES/blob/9d948c3bd8364de057d0484047cc13eb1c360f04/recuperaciones/unidad_4/actividadesClase/ejercicio1.php" target="_blank">Enlace a GitHub</a>
     </footer>
 </body>
 </html>
