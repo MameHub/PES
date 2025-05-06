6e420dab36c3b1602ab1e22937d4f4780d3fd7ba
Álvaro Cañas <acaygo@gmail.com>
2025-05-06 03:20:16 +0200

    login

--- recuperaciones/unidad_4/1_peliculas/login.php
+++ recuperaciones/unidad_4/1_peliculas/login.php
@@ -3,19 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style/style.css">
     <title>Document</title>
 </head>
 <body>
     <div class="login">
         <form action="index.php" method="post">
-            <div class="user">
-                <label for="user">Usuario</label>
+            <!-- <div class="user"> -->
+                <label for="user">Usuario:</label>
                 <input type="text" name="" id="user" placeholder="Usuario">
-            </div>
-            <div class="pass">
-                <label for="pass">Contraseña</label>
+            <!-- </div>
+            <div class="pass"> -->
+                <label for="pass">Contraseña:</label>
                 <input type="password" name="" id="pass" placeholder="Contraseña">
-            </div>
+            <!-- </div> -->
         </form>
     </div>
 </body>
